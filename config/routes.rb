c8358f1c030b816bafc797d218423bc3f6488038
Craig <craig@stembolt.com>
2018-05-01 10:28:14 -0700

    Add controller route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Spree::Core::Engine.routes.draw do
   namespace 'api' do
-    post 'pos_order' => 'pos_order#create', as: :create
+    post 'pos_order' => 'pos_order#create', as: :create, defaults: { format: 'json' }
   end
   # Add your extension routes here
 end
